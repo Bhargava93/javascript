3d7457deddc89c3504944e58e1f83787144ff3ce
bhargava <bhargavahara@gmail.com>
2023-04-14 20:43:55 +0530

    array and object

---
+++ array.js
@@ -0,0 +1,134 @@
+
+var firstname="bhargava";
+
+//multiple values
+var names=["bharagava","srikanth","dhana","guru"]; 
+
+// irrespective of datatype any value we can store
+var age= 10;
+
+var names=["bharagava","srikanth","dhana","guru",10,20,]; 
+
+var details=["bharagava","srikanth","dhana","guru",10,20,"hindupur","kadiri","tirupati"]; 
+
+// console.log(details.length-1)
+// console.log(details[details.length-1]);
+
+// console.log(details[0]);
+// console.log(details[1]);
+// console.log(details[2]);
+// console.log(details[3]);
+// console.log(details[4])
+// console.log(details[5])
+// console.log(details[6])
+// console.log(details[7])
+// console.log(details[8])
+
+// index == 0
+// details.length
+
+// for(var i=0;i<details.length;i++){
+//  console.log(details[i]);
+// }
+
+
+// for(var i=0;i<details.length;i++){
+// console.log(details[i]);
+// }
+
+
+var details=["bharagava","srikanth","dhana","guru",10,20,"hindupur"];
+var age=[1,100,300,40,50];
+var city=["bangalore","dehli","vizag"];
+
+
+// to combain the two arrays we will use concat()
+// concat() 
+// var combain=age.concat(details,city)
+// console.log(combain);
+
+// tofind index postion for element
+// indexOf
+// details.indexOf("dhana");
+// console.log(details.indexOf("dhana"));
+
+// console.log("before push==",details);
+//element will remove last postion of the array
+// details.pop();
+
+//element will remove first postion of the array
+// details.shift();
+// console.log("after push==",details);
+
+//element added into the last postion
+// use push()
+// details.push("dhana");
+// details.push("guru");
+// details.push("swetha");
+// console.log("after====",details);
+
+//elements added starting of the array
+// details.unshift("harathi");
+// details.unshift("srikanth");
+// console.log("unshift====",details);
+
+
+
+
+
+
+// function funactionName(monday){
+
+//     return;
+// }
+
+// funactionName();
+
+// function printDaysOfWeek(week) {
+
+//     // console.log("week=====",week);
+//     const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+//     for (let i = 0; i < daysOfWeek.length; i++) {
+//         // console.log(daysOfWeek[i]);
+
+//         if(week.toLowerCase() === daysOfWeek[i].toLowerCase()){
+//             return "today is "+daysOfWeek[i];
+//         }
+
+//     }
+    
+// }
+
+// var result = printDaysOfWeek("MONDAY");
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function printDaysOfWeek() {
+
+//     const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+//     for (let i = 0; i < daysOfWeek.length; i++) {
+
+//         console.log(daysOfWeek[i]);
+
+//     }
+
+// }
+
+// printDaysOfWeek("to day is");
+
---
+++ object.js
@@ -0,0 +1,97 @@
+// Object
+
+// key value pair
+
+// var details_obj={key:value}
+
+// var details_obj = {
+//     "name": "bhargava",
+//     "collage": "jntuv",
+//     "city": "bangalore",
+//     "datails": {
+//         "education": "mca",
+//         "job": "sd2",
+//         "location": "bangalore",
+//         "company": {
+//             "comapnyname": "haapiest",
+//             "location": "bangalore"
+//         }
+//     },
+//     "family details": {
+//         "fathername": "raja",
+//         "mothername": "tulasi"
+//     },
+//     "town":"bangalore"
+
+// }
+
+// "bangalore"
+
+// for(var i=0;i<Object.keys(details_obj).length;i++){
+
+//     // console.log(i);
+//     if(details_obj[Object.keys(details_obj)[i]] === "bangalore"){
+        
+//         console.log(Object.keys(details_obj)[i]);
+    
+//     }
+
+// }
+
+
+// remove values from object
+// delete details_obj["name"]
+// delete details_obj["collage"]
+
+// console.log(details_obj);
+
+// 1st method
+// console.log(details_obj.name);
+// console.log(details_obj.collage);
+// console.log(details_obj.city);
+
+// 2nd method
+// details_obj["city"];
+// details_obj["name"];
+// details_obj["collage"];
+// details_obj["datails"];
+// details_obj["family details"]
+
+
+// var keys = Object.keys(details_obj);
+
+// console.log("keys===", keys);
+// console.log("keys length===", keys.length);
+
+// for (var i = 0; i < keys.length; i++) {
+
+    // find index postion
+    // console.log(i);
+
+    // keys find
+    // console.log(keys[i]);
+
+    // values find
+    // console.log(details_obj[keys[i]]);
+
+// }
+
+// var values= Object.values(details_obj);
+//
+// console.log("values===",values);
+
+// .
+// details_obj.name;
+// console.log(details_obj.name);
+// console.log(details_obj["name"]);
+// console.log(details_obj["family details"]);
+
+// details_obj.name1="srikanth";
+
+// console.log(details_obj);
+
+// var firstname="guru";
+
+// firstname="prasnna";
+
+// console.log(firstname);
