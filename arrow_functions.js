8dc49a757ad55b9d29da0a041c9caa6e3556b37d
bhargava <bhargavahara@gmail.com>
2023-04-23 20:37:26 +0530

    arrow functions and this

---
+++ arrow_functions.js
@@ -0,0 +1,48 @@
+// // function declaration
+// function sample1(){
+
+// }
+
+// sample1();
+
+// // function expression
+// let sample2 = function () {
+//     return;
+// }
+
+// sample2();
+
+
+// arrow functions
+
+// let sample = (a, b) => a + b;
+
+// let sample1 = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
+
+
+// console.log(sample(1, 2));
+
+// this
+
+// let person={
+//     "name":"bhargava",
+//     "id":1,
+//     "collage":"jntu",
+
+//     "sample":function(){
+//         // console.log("inside this===",this.name);
+//         return this;
+//     },
+
+//     sample1(){
+//         return this;
+//     }
+// }
+// console.log("person===",person.sample1());
+
+// console.log(Object.keys(person));
+
+// console.log(this);
---
+++ destructure.js
@@ -0,0 +1,75 @@
+// let first_name="bhargava";
+// let collage="jntu";
+// let company="happiestminds";
+
+// let array=[];
+// let obj={}
+
+// let first_name = "bhargava", collage="jntu",company= "happiestminds";
+
+// let first_name, collage, company;
+
+// let first_name = collage = company = 10;
+
+// console.log("first_name===",first_name);
+// console.log("collage===",collage);
+// console.log("company===",company);
+
+// let deatils = ["bhargava", "jntu", "happiestminds", "bangalore"];
+// // console.log("details===",deatils);
+
+// // array destructing
+// let [name, collage, ...reaming_values] = ["bhargava", "jntu", "happiestminds", "bangalore"];
+
+// // console.log("name===", name);
+// console.log("reaming_values===", reaming_values);
+// // console.log("company===", company);
+
+//  let [company, city]=reaming_values;
+// console.log("city===", city);
+// console.log("company===",company);
+
+// let sample = function(...details){
+//     console.log("details===",details);
+//     // let [name,collage]=details;
+
+//     let name = details[0];
+//     let collage=details[1];
+
+//     console.log("name===",name);
+//     console.log("collage===",collage);
+
+//     return;
+// }
+
+// sample("bhargava","jntu","hindupur","happiest");
+
+
+// object
+// Key, value
+
+let deatils_obj = {
+    "name": "bhargava",
+    "collage": "jntu",
+    "company": "happiestminds",
+    "city_of": "bangalore"
+};
+
+
+// console.log("name===", deatils_obj.name);
+// console.log("name===", deatils_obj["name"]);
+
+// let { name,  company, ...obj}=deatils_obj;
+
+// console.log(name);
+// console.log("obj==",obj);
+// console.log(company);
+// console.log(city_of);
+
+let sample = function({name, collage,...obj}){
+console.log(name, collage, obj);
+return obj;
+}
+// sample(deatils_obj);
+
+module.exports=sample;
